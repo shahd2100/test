b5e256ef27dd574063cdc7b13aadda48cc774d15
shahd2100 <143960248+shahd2100@users.noreply.github.com>
2024-02-08 01:55:17 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        dots:true,
+        animateOut: 'fadeOut',
+    });
+  });
